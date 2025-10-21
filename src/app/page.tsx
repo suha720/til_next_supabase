b9a4f40a929a0c8734fb95e46914d8d09891b3d4
suha <a50914513@gmail.com>
2025-10-21 13:23:09 +0900

    [docs] react query 설정 및 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
-import Counter from '@/components/Counter';
-import ThemeToggle from '@/components/ThemeToggle';
-import TodoList from '@/components/TodoList';
-import UserProfile from '@/components/UserProfile';
 
 export default function Home() {
   return (
     <div>
-      {/* <ButtonTest />
-      <SCSSTest /> */}
-      <Counter />
-      <br />
-      <UserProfile />
-      <br />
-      <ThemeToggle />
-      <br />
-      <TodoList />
+      <h2>React Query</h2>
     </div>
   );
 }
