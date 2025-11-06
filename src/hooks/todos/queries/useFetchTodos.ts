8005868978d7dc52211fd5bb2e6ecb7a61ee8531
suha <a50914513@gmail.com>
2025-11-06 11:55:26 +0900

    [docs] 데이터 무효화 및 낙관적 업데이트

--- src/hooks/todos/queries/useFetchTodos.ts
+++ src/hooks/todos/queries/useFetchTodos.ts
@@ -1,9 +1,10 @@
 import { fetchTodos } from '@/apis/todo';
+import { QUERY_KEYS } from '@/lib/constants';
 import { useQuery } from '@tanstack/react-query';
 
 export function useFetchTodos() {
   return useQuery({
-    queryKey: ['todos'], // 저장소 이름
+    queryKey: QUERY_KEYS.todo.list, // 저장소 이름
     queryFn: fetchTodos,
     staleTime: 5 * 1000,
   });
