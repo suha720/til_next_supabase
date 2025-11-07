57d2dce97654e67cfaf2bc9da832ec469114f273
suha <a50914513@gmail.com>
2025-11-07 10:34:58 +0900

    [docs] 데이터 무효화 및 낙관적 업데이트

--- src/hooks/mutations/useUpdateTodoMutation.ts
+++ src/hooks/mutations/useUpdateTodoMutation.ts
@@ -9,7 +9,15 @@ export function useUpdateTodoMutation() {
   return useMutation({
     mutationFn: updateTodo,
     // mutate 함수가 실행되는 시점에 작성
-    onMutate: updatedTodo => {
+    onMutate: async updatedTodo => {
+      // 요청 취소 기능 구현
+      await queryClient.cancelQueries({ queryKey: QUERY_KEYS.todo.list });
+
+      // 원본 데이터를 보관함.
+      const originTodos = queryClient.getQueryData<Todo[]>(
+        QUERY_KEYS.todo.list
+      );
+
       // 캐시 데이터를 업데이트 기능 작성
       // 2. 데이터 일부만 수정함.
       queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, prevTodos => {
@@ -18,6 +26,26 @@ export function useUpdateTodoMutation() {
           todo.id === updatedTodo.id ? { ...todo, ...updatedTodo } : todo
         );
       });
+
+      return { originTodos };
+    },
+    onError: (error, valiable, context) => {
+      console.log(error);
+
+      // 원상복구를 위해서 context 에 보관해둔 값으로 갱신한다.
+      if (context?.originTodos) {
+        queryClient.setQueryData<Todo[]>(
+          QUERY_KEYS.todo.list,
+          context.originTodos
+        );
+      }
+
+      return { error };
+    },
+    // 요청이 완료됨
+    onSettled: () => {
+      // 검증 과정
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.todo.list });
     },
   });
 }
