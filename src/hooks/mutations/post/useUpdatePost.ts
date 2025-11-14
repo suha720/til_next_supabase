5f4a14e6f914e3c07f727f91b44d696a76094e80
suha <a50914513@gmail.com>
2025-11-14 15:24:49 +0900

    [docs] 포스트 수정

---
+++ src/hooks/mutations/post/useUpdatePost.ts
@@ -0,0 +1,16 @@
+// import { UseMuationCallback } from '@/types/types';
+import { useMutation } from '@tanstack/react-query';
+import { updatePost } from '@/apis/post';
+import { UseMutationCallback } from '@/types/types';
+
+export function useUpdatePost(callback?: UseMutationCallback) {
+  return useMutation({
+    mutationFn: updatePost,
+    onSuccess: () => {
+      if (callback?.onSuccess) callback.onSuccess();
+    },
+    onError: error => {
+      if (callback?.onError) callback.onError(error);
+    },
+  });
+}
